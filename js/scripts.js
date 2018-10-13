06d8838175490a83567135fdd87138b51bf20312
Anna Rusyayeva <annarusyayeva@MBP-Rusyayeva.Dlink>
2018-10-14 02:25:28 +0500

    test swipe

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+import Swiper from 'swiper.min';
+
 let experienceItemCard11 = document.querySelector('.experience__item--card-1-1');
 let experienceItemCard12 = document.querySelector('.experience__item--card-1-2');
 
@@ -23,3 +25,25 @@ experienceItemSwitcher12.addEventListener('click', function(evt) {
         experienceItemSwitcher12.classList.toggle('experience__item--switcher-active');
     }
 });
+
+let swiper1 = new Swiper('.swiper-container', {
+pagination: {
+el: '.swiper-pagination',
+},
+navigation: {
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
+},
+});
+let swiper2 = new Swiper('.swiper2', {
+pagination: {
+el: '.swiper-pagination2',
+},
+navigation: {
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
+},
+});
+};
+
+
