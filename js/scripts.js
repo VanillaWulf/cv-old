b8a3dc5fb47492f9fcb8fd73ba3725778b0ca0c2
Anna Rusyayeva <annarusyayeva@MBP-Rusyayeva.Dlink>
2018-10-14 02:58:24 +0500

    slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-import Swiper from 'swiper.min';
 
 let experienceItemCard11 = document.querySelector('.experience__item--card-1-1');
 let experienceItemCard12 = document.querySelector('.experience__item--card-1-2');
@@ -25,25 +24,5 @@ experienceItemSwitcher12.addEventListener('click', function(evt) {
         experienceItemSwitcher12.classList.toggle('experience__item--switcher-active');
     }
 });
-
-let swiper1 = new Swiper('.swiper-container', {
-pagination: {
-el: '.swiper-pagination',
-},
-navigation: {
-nextEl: '.swiper-button-next',
-prevEl: '.swiper-button-prev',
-},
-});
-let swiper2 = new Swiper('.swiper2', {
-pagination: {
-el: '.swiper-pagination2',
-},
-navigation: {
-nextEl: '.swiper-button-next',
-prevEl: '.swiper-button-prev',
-},
-});
-};
 
 
