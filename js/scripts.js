f3b5fa528f95517ac5842f30d34fcf14e7d848a4
Anna Rusyayeva <annarusyayeva@MBP-Rusyayeva.Dlink>
2018-10-14 13:41:56 +0500

    swiper

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,16 @@
-
-let experienceItemCard11 = document.querySelector('.experience__item--card-1-1');
-let experienceItemCard12 = document.querySelector('.experience__item--card-1-2');
-
-let experienceItemSwitcher11 = document.getElementById('experience__item--switcher-1-1');
-let experienceItemSwitcher12 = document.getElementById('experience__item--switcher-1-2');
-
-experienceItemSwitcher11.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    if (experienceItemCard12.classList.contains('display-block')) {
-        experienceItemCard11.classList.toggle('display-block');
-        experienceItemCard12.classList.toggle('display-block');
-        experienceItemSwitcher11.classList.toggle('experience__item--switcher-active');
-        experienceItemSwitcher12.classList.toggle('experience__item--switcher-active');
-    }
+$(document).ready(function () {
+    //initialize swiper when document ready
+    let expirienceSwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        pagination: {
+                    el: '.swiper-pagination',
+                    type: 'bullets',
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+    })
 });
-
-experienceItemSwitcher12.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    if (experienceItemCard11.classList.contains('display-block')) {
-        experienceItemCard11.classList.toggle('display-block');
-        experienceItemCard12.classList.toggle('display-block');
-        experienceItemSwitcher11.classList.toggle('experience__item--switcher-active');
-        experienceItemSwitcher12.classList.toggle('experience__item--switcher-active');
-    }
-});
-
 
